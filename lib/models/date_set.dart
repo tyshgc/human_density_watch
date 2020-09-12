a1eaaccad2f38c2558b9cae54b48d40e64a64551
tyshgc <hello@tyshgc.pw>
2020-09-12 13:30:56 +0900

    Enhanced: added seconds, padMinites, padSeconds on DateSet Model.

--- lib/models/date_set.dart
+++ lib/models/date_set.dart
@@ -1,23 +1,57 @@
 import 'package:intl/intl.dart';
 
 /// ### DateSet Model
+/// ---
+/// 日付・日時に関するモデル
 class DateSet {
   final DateTime date;
   final LOCALE lang;
 
   DateSet(this.date, this.lang);
 
-  String get year => DateFormat.y(lang.value).format(dateObject());
-  String get month => DateFormat.M(lang.value).format(dateObject());
-  String get day => DateFormat.d(lang.value).format(dateObject());
-  String get hour => DateFormat.H(lang.value).format(dateObject());
-  String get minites => DateFormat.m(lang.value).format(dateObject());
+  String get year => getDateElement(DATE_ELEMENT.YEAR);
+  String get month => getDateElement(DATE_ELEMENT.MONTH);
+  String get day => getDateElement(DATE_ELEMENT.DAY);
+  String get hour => getDateElement(DATE_ELEMENT.HOUR);
+  String get minites => getDateElement(DATE_ELEMENT.MINITES);
+  String get seconds => getDateElement(DATE_ELEMENT.SECONDS);
+  String get padMinites => getDateElement(DATE_ELEMENT.MINITES).padLeft(2, "0");
+  String get padSeconds => getDateElement(DATE_ELEMENT.SECONDS).padLeft(2, "0");
 
-  DateTime dateObject() {
+  String getDateElement(DATE_ELEMENT type) {
+    DateFormat _local;
+    DateTime _date = getDateObject();
+
+    switch (type) {
+      case DATE_ELEMENT.YEAR:
+        _local = DateFormat.y(lang.value);
+        break;
+      case DATE_ELEMENT.MONTH:
+        _local = DateFormat.M(lang.value);
+        break;
+      case DATE_ELEMENT.DAY:
+        _local = DateFormat.d(lang.value);
+        break;
+      case DATE_ELEMENT.HOUR:
+        _local = DateFormat.H(lang.value);
+        break;
+      case DATE_ELEMENT.MINITES:
+        _local = DateFormat.m(lang.value);
+        break;
+      case DATE_ELEMENT.SECONDS:
+        _local = DateFormat.s(lang.value);
+        break;
+    }
+
+    return _local.format(_date);
+  }
+
+  DateTime getDateObject() {
     return date != null ? date : DateTime.now();
   }
 }
 
+enum DATE_ELEMENT { YEAR, MONTH, DAY, HOUR, MINITES, SECONDS }
 enum LOCALE { JP, EN }
 
 extension LOCALE_STRING on LOCALE {
