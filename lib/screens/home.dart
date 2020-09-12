d2ca067ccb56d22bdb13d4ba30612e03e0354ed5
tyshgc <hello@tyshgc.pw>
2020-09-12 13:37:43 +0900

    Fixed: put TimerText Widget on Home Screen Widget.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:human_density_watch/widgets/bluetooth.dart';
 import 'package:human_density_watch/widgets/backgroud.dart';
 import 'package:human_density_watch/widgets/date_view.dart';
+import 'package:human_density_watch/widgets/timer_texts.dart';
+import 'package:human_density_watch/models/seconds.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key key, this.title}) : super(key: key);
@@ -22,7 +24,8 @@ class HomeScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Bluetooth(),
-              DateView(targetDate: DateTime(2009, 5, 6))
+              DateView(targetDate: DateTime(2009, 5, 6)),
+              TimerTexts(time: Seconds(20.2))
             ],
           ),
         ),
